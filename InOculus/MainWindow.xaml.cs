23f5a91e2c5f6751e5ead0430655c141332dc891
Jana Rezabkova <jana.rezabkova@gmail.com>
2022-10-02 20:57:55 +0200

    Bind timer

--- InOculus/MainWindow.xaml.cs
+++ InOculus/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using InOculus.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.UI.ViewManagement;
 
 namespace InOculus
 {
@@ -20,10 +23,35 @@ namespace InOculus
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IntervalTimer IntervalTimer = new IntervalTimer();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeColorTheme();
+            lblTime.DataContext = IntervalTimer;
         }
 
+        private void InitializeColorTheme()
+        {
+            var settings = new UISettings();
+            var accent = settings.GetColorValue(UIColorType.Accent);
+            var background = settings.GetColorValue(UIColorType.Background);
+
+            var accentColor = Color.FromArgb(accent.A, accent.R, accent.G, accent.B);
+            var accentBrush = new SolidColorBrush(accentColor);
+            var backgroundColor = Color.FromArgb(background.A, background.R, background.G, background.B);
+            var backgroundBrush = new SolidColorBrush(backgroundColor);
+             
+            btnStart.Background = accentBrush;
+            btnSettings.Background = accentBrush;
+            wndInOculus.Background = backgroundBrush;
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            IntervalTimer.Start();
+        }
     }
 }
---
+++ InOculus/Utilities/IntervalTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Timers;
+
+namespace InOculus.Utilities
+{
+    class IntervalTimer: INotifyPropertyChanged
+    {
+        private Timer timer = new Timer(1000);
+        public event PropertyChangedEventHandler PropertyChanged;
+        public TimeSpan CountDown { get; private set; } = new TimeSpan(hours: 0, minutes: Properties.Settings.Default.Interval, seconds: 0);
+
+        public IntervalTimer()
+        {
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            CountDown += TimeSpan.FromSeconds(-1);
+            PropertyChanged(this, new PropertyChangedEventArgs(nameof(CountDown)));
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+    }
+}
