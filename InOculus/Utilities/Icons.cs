7ecb411121e1c8fa87d39bafd36f95d3ebde9f70
Jana Rezabkova <jana.rezabkova@gmail.com>
2022-10-03 21:30:42 +0200

    List icon choices in util file

---
+++ InOculus/Utilities/Icons.cs
@@ -0,0 +1,12 @@
+﻿using MahApps.Metro.IconPacks;
+using static MahApps.Metro.IconPacks.PackIconBootstrapIconsKind;
+
+namespace InOculus.Utilities
+{
+    public static class Icons
+    {
+        public const PackIconBootstrapIconsKind Play = PlayFill;
+        public const PackIconBootstrapIconsKind Stop = StopFill;
+        public const PackIconBootstrapIconsKind Settings = Gear;
+    }
+}
